e6bd3fedc5737ce1ea332c4cc6065613fab34565
shimizu666 <1553362138@qq.com>
2017-06-12 18:59:28 +0800

    Initial Commit

---
+++ YingKe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YingKe
+//
+//  Created by 邓康大 on 2017/6/12.
+//  Copyright © 2017年 邓康大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
